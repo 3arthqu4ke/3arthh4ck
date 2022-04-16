5dd3446528686b425a2235b2d3c708c461237f72
3arthqu4ke <hanspeteroliver.mueller@gmail.com>
2022-04-16 23:34:26 +0200

    bump to 1.5.0

--- src/main/java/me/earth/earthhack/impl/Earthhack.java
+++ src/main/java/me/earth/earthhack/impl/Earthhack.java
@@ -14,7 +14,7 @@ public class Earthhack implements Globals
 {
     private static final Logger LOGGER = LogManager.getLogger("3arthh4ck");
     public static final String NAME = "3arthh4ck";
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.5.0";
 
     public static void preInit()
     {
