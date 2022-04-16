a0e632b67d56c7ca8557cd06a725d8f02d430455
Ozynt <58683893+Ozynt@users.noreply.github.com>
2022-04-16 21:33:29 +0200

    Updated setting name

--- src/main/java/me/earth/earthhack/impl/modules/movement/speed/Speed.java
+++ src/main/java/me/earth/earthhack/impl/modules/movement/speed/Speed.java
@@ -68,7 +68,7 @@ public class Speed extends Module
     protected final Setting<Boolean> noWaterInstant =
             register(new BooleanSetting("NoLiquidInstant", false));
     protected final Setting<Boolean> Sneak =
-            register(new BooleanSetting("Sneak", false));
+            register(new BooleanSetting("SneakInstant", false));
 
     protected final StopWatch expTimer = new StopWatch();
 
