f3c23591c5b1d073359d3383e31d24ee99fde3e2
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2022-08-23 23:21:46 +0200

    [1.8.0] Bump version

--- src/main/java/me/earth/earthhack/impl/Earthhack.java
+++ src/main/java/me/earth/earthhack/impl/Earthhack.java
@@ -15,7 +15,7 @@ public class Earthhack implements Globals
 {
     private static final Logger LOGGER = LogManager.getLogger("3arthh4ck");
     public static final String NAME = "3arthh4ck";
-    public static final String VERSION = "1.7.14";
+    public static final String VERSION = "1.8.0";
 
     public static void preInit()
     {
