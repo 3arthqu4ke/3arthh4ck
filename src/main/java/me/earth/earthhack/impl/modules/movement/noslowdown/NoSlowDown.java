d76f43110ac0f5b391e13d7039be49558873f9c6
cattyn <cvenby@gmail.com>
2022-08-28 23:13:23 +0300

    Fixed NoSlowDown compatibility with step timer

--- src/main/java/me/earth/earthhack/impl/modules/movement/noslowdown/NoSlowDown.java
+++ src/main/java/me/earth/earthhack/impl/modules/movement/noslowdown/NoSlowDown.java
@@ -56,6 +56,7 @@ public class NoSlowDown extends Module
 
     protected final KeyBinding[] keys;
     protected boolean spoof = true;
+    protected boolean usingTimer;
 
     public NoSlowDown()
     {
