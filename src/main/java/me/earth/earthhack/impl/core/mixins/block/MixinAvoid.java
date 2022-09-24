7f8719edb855e4e9e875c24f15fec3e989d00dd7
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2022-09-24 11:30:10 +0200

    [1.8.5] Lambda compatibility #296

--- src/main/java/me/earth/earthhack/impl/core/mixins/block/MixinAvoid.java
+++ src/main/java/me/earth/earthhack/impl/core/mixins/block/MixinAvoid.java
@@ -1,11 +1,11 @@
 package me.earth.earthhack.impl.core.mixins.block;
 
 import me.earth.earthhack.api.cache.ModuleCache;
-import me.earth.earthhack.api.util.interfaces.Globals;
 import me.earth.earthhack.impl.modules.Caches;
 import me.earth.earthhack.impl.modules.movement.avoid.Avoid;
 import net.minecraft.block.*;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -22,9 +22,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
     BlockCactus.class,
     BlockLiquid.class
 })
-public abstract class MixinAvoid implements Globals {
+public abstract class MixinAvoid {
     private static final ModuleCache<Avoid> AVOID =
         Caches.getModule(Avoid.class);
+    private final Minecraft mc = Minecraft.getMinecraft();
 
     @Inject(
         method = "getCollisionBoundingBox",
